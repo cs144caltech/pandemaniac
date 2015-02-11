98a14ffdafdbe9909b989ae31fd36efbb4179701
Chung Eun Kim <kce0423@gmail.com>
2015-02-11 15:01:07 -0800

    scripts added

---
+++ init-pandemaniac.sh
@@ -0,0 +1,11 @@
+rm -f pandemaniac-graphui/private/runs/*
+find pandemaniac-graphui/private/uploads -mindepth 1 -maxdepth 1 -type d \
+				         -exec rm -rf '{}' +
+
+source copy-ta-graphs.sh
+
+./mongo localhost:27017/test --eval "db.teams.drop()"
+./mongo localhost:27017/test --eval "db.attempts.drop()"
+./mongo localhost:27017/test --eval "db.runs.drop()"
+./mongo localhost:27017/test pandemaniac-graphui/setup/setup-graphs.js
+./mongo localhost:27017/test pandemaniac-graphui/setup/ensure-indices.js
---
+++ start-pandemaniac.sh
@@ -0,0 +1,11 @@
+./redis-server redis.conf &
+echo $! > redis-server.pid
+
+./mongod --config mongodb.conf &
+echo $! > mongod.pid
+
+# Need to change directories so that relative path is correct
+cd pandemaniac-graphui
+NODE_ENV=production ./node app.js &
+cd ../
+echo $! > node.pid
