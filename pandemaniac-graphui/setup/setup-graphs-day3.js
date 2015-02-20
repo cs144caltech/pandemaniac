248d0693e60cfe0887919345bfc9ff84e7419345
Jianchi Chen <jchen2@caltech.edu>
2015-02-19 22:46:28 -0800

    all graphs generated and added; graph setup files updated; still need TA team submissions

--- pandemaniac-graphui/setup/setup-graphs-day3.js
+++ pandemaniac-graphui/setup/setup-graphs-day3.js
@@ -1,13 +1,13 @@
 var category = "day3"
-  , start = ISODate("2014-02-22T00:00:00.000-08:00Z")
-  , end = ISODate("2014-02-23T00:00:00.000-08:00Z");
+  , start = ISODate("2015-02-22T00:00:00.000-08:00Z")
+  , end = ISODate("2015-02-23T00:00:00.000-08:00Z");
 
 // NOTE: variables `minute` and `descriptions` defined in setup-graphs.js
 
 db.graphs.insert({
   name: "2.5.3"
 , category: category
-, file: "email-Enron-forest-fire-100-0.6-53.json"
+, file: "2.5.3.json"
 , timeout: minute * 3
 , start: start
 , end: end
@@ -15,7 +15,7 @@ db.graphs.insert({
 db.graphs.insert({
   name: "4.5.3"
 , category: category
-, file: "email-Enron-forest-fire-200-0.6-86.json"
+, file: "4.5.3.json"
 , timeout: minute * 3
 , start: start
 , end: end
@@ -23,7 +23,7 @@ db.graphs.insert({
 db.graphs.insert({
   name: "4.10.3"
 , category: category
-, file: "email-Enron-forest-fire-500-0.6-66.json"
+, file: "4.10.3.json"
 , timeout: minute * 3
 , start: start
 , end: end
@@ -31,7 +31,7 @@ db.graphs.insert({
 db.graphs.insert({
   name: "2.10.12"
 , category: category
-, file: "email-Enron-forest-fire-500-0.6-199.json"
+, file: "2.10.12.json"
 , timeout: minute * 5
 , start: start
 , end: end
@@ -40,7 +40,7 @@ db.graphs.insert({
 db.graphs.insert({
   name: "2.10.22"
 , category: category
-, file: "email-Enron-forest-fire-500-0.6-80.json"
+, file: "2.10.22.json"
 , timeout: minute * 5
 , start: start
 , end: end
@@ -49,16 +49,16 @@ db.graphs.insert({
 db.graphs.insert({
   name: "2.10.32"
 , category: category
-, file: "email-Enron-forest-fire-500-0.6-90.json"
+, file: "2.10.32.json"
 , timeout: minute * 5
 , start: start
 , end: end
-, desc: descriptions.eyeball
+, desc: descriptions.more
 });
 db.graphs.insert({
   name: "8.10.3"
 , category: category
-, file: "email-Enron-forest-fire-1000-0.6-75.json"
+, file: "8.10.3.json"
 , timeout: minute * 5
 , start: start
 , end: end
@@ -66,7 +66,7 @@ db.graphs.insert({
 db.graphs.insert({
   name: "8.20.4"
 , category: category
-, file: "email-Enron-forest-fire-2000-0.6-23.json"
+, file: "8.20.4.json"
 , timeout: minute * 5
 , start: start
 , end: end
@@ -74,7 +74,7 @@ db.graphs.insert({
 db.graphs.insert({
   name: "8.30.1"
 , category: category
-, file: "twitter_combined-forest-fire-5000-0.6-14.json"
+, file: "8.30.1.json"
 , timeout: minute * 5
 , start: start
 , end: end
@@ -82,7 +82,7 @@ db.graphs.insert({
 db.graphs.insert({
   name: "8.40.1"
 , category: category
-, file: "Slashdot0902-forest-fire-10000-0.6-18.json"
+, file: "8.40.1.json"
 , timeout: minute * 5
 , start: start
 , end: end
--- pandemaniac-graphui/setup/setup-graphs-day4.js
+++ pandemaniac-graphui/setup/setup-graphs-day4.js
@@ -1,13 +1,13 @@
 var category = "day4"
-  , start = ISODate("2014-02-23T00:00:00.000-08:00Z")
-  , end = ISODate("2014-02-24T00:00:00.000-08:00Z");
+  , start = ISODate("2015-02-23T00:00:00.000-08:00Z")
+  , end = ISODate("2015-02-24T00:00:00.000-08:00Z");
 
 // NOTE: variables `minute` and `descriptions` defined in setup-graphs.js
 
 db.graphs.insert({
   name: "2.5.4"
 , category: category
-, file: "email-Enron-forest-fire-100-0.6-88.json"
+, file: "2.5.4.json"
 , timeout: minute * 3
 , start: start
 , end: end
@@ -15,7 +15,7 @@ db.graphs.insert({
 db.graphs.insert({
   name: "4.5.4"
 , category: category
-, file: "email-Enron-forest-fire-200-0.6-69.json"
+, file: "4.5.4.json"
 , timeout: minute * 3
 , start: start
 , end: end
@@ -23,7 +23,7 @@ db.graphs.insert({
 db.graphs.insert({
   name: "4.10.4"
 , category: category
-, file: "email-Enron-forest-fire-500-0.6-33.json"
+, file: "4.10.4.json"
 , timeout: minute * 3
 , start: start
 , end: end
@@ -31,7 +31,7 @@ db.graphs.insert({
 db.graphs.insert({
   name: "2.10.13"
 , category: category
-, file: "email-Enron-forest-fire-500-0.6-308.json"
+, file: "2.10.13.json"
 , timeout: minute * 5
 , start: start
 , end: end
@@ -40,7 +40,7 @@ db.graphs.insert({
 db.graphs.insert({
   name: "2.10.23"
 , category: category
-, file: "email-Enron-forest-fire-500-0.6-62.json"
+, file: "2.10.23.json"
 , timeout: minute * 5
 , start: start
 , end: end
@@ -49,16 +49,16 @@ db.graphs.insert({
 db.graphs.insert({
   name: "2.10.33"
 , category: category
-, file: "email-Enron-forest-fire-500-0.6-101.json"
+, file: "2.10.33.json"
 , timeout: minute * 5
 , start: start
 , end: end
-, desc: descriptions.eyeball
+, desc: descriptions.more
 });
 db.graphs.insert({
   name: "8.10.4"
 , category: category
-, file: "email-Enron-forest-fire-1000-0.6-54.json"
+, file: "8.10.4.json"
 , timeout: minute * 5
 , start: start
 , end: end
@@ -66,7 +66,7 @@ db.graphs.insert({
 db.graphs.insert({
   name: "8.20.5"
 , category: category
-, file: "email-Enron-forest-fire-2000-0.6-58.json"
+, file: "8.20.5.json"
 , timeout: minute * 5
 , start: start
 , end: end
@@ -74,7 +74,7 @@ db.graphs.insert({
 db.graphs.insert({
   name: "8.25.2"
 , category: category
-, file: "twitter_combined-forest-fire-5000-0.6-6.json"
+, file: "8.25.2.json"
 , timeout: minute * 5
 , start: start
 , end: end
@@ -82,7 +82,7 @@ db.graphs.insert({
 db.graphs.insert({
   name: "8.40.2"
 , category: category
-, file: "twitter_combined-forest-fire-10000-0.6-10.json"
+, file: "8.40.2.json"
 , timeout: minute * 5
 , start: start
 , end: end
