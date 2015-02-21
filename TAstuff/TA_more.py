7666658cb11a382f9fae8b06e12d57fc8d14862e
Jianchi Chen <jchen2@caltech.edu>
2015-02-21 01:51:11 -0800

    TA more changed

--- TAstuff/TA_more.py
+++ TAstuff/TA_more.py
@@ -10,12 +10,15 @@ if __name__ == "__main__":
 	for i in range(5):
 		with open(graphdir+"2.10.3%d.json" % i, 'r') as g:
 			graph = json.loads(''.join(g.readlines()))
-		d = compute.get_top_N(graph, "degree", 20)
-		c = compute.get_top_N(graph, "betweenness", 20)
-		nodeset = set(d+c)
+		d = compute.get_top_N(graph, "degree", 12)
+		b = compute.get_top_N(graph, "betweenness", 15)
+		c = compute.get_top_N(graph, "closeness", 15)
+		nodeset = set(b+c)
 		result = []
 		for _ in range(50):
-			result += random.sample(nodeset, 12)
+			extra = random.sample(nodeset, 6)
+			curset = random.sample(set(extra+d), 12)
+			result += curset
 		with open(outdir+"2.10.3%d.txt" % i, 'w') as of:
 			for node in result:
 				of.write("%s\n" % node)
